23afc29fa8ec63e522be8e39f92945ff74b4c49a
Tyler Pugh <tyler.pugh@rigup.com>
2020-02-28 14:16:26 -0600

    again test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Does this work? Maybe. Hi there! I am Tyler. Cool beans, Kubernetes Deployments manage stateless services running on your cluster. Their purpose is to keep a set of identical pods running and upgrade them in a controlled way – performing a rolling update by default\n');
+    response.end('Does this work? Maybe. Hi there! I am Tyler. The best! Cool beans, Kubernetes Deployments manage stateless services running on your cluster. Their purpose is to keep a set of identical pods running and upgrade them in a controlled way – performing a rolling update by default\n');
 }).listen(8030);
 
 console.log('Server started');
