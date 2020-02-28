fc19eafa4983be2159d363726ac1869165831238
Tyler Pugh <tyler.pugh@rigup.com>
2020-02-28 11:49:20 -0600

    fix config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('A Kubernetes Deployments manage stateless services running on your cluster. Their purpose is to keep a set of identical pods running and upgrade them in a controlled way – performing a rolling update by default\n');
+    response.end('A thing Kubernetes Deployments manage stateless services running on your cluster. Their purpose is to keep a set of identical pods running and upgrade them in a controlled way – performing a rolling update by default\n');
 }).listen(8030);
 
 console.log('Server started');
